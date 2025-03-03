bb7a941d3f3516ca98fe0dad2778304eb858fd6f
Michael <mpborer@gmail.com>
2025-03-02 19:24:15 -0700

    java/objects

--- app.js
+++ app.js
@@ -12,27 +12,50 @@ let users = [
     password: `mitri123`,
     email: `Mitri@gmail.com`,
     subscriptionStatus: `VIP`,
-    githubID: `Mitrirer`,
+    githubID: `Mitri000`,
     lessonsCompleted: [0, 1, 2, 3],
 },
 {
     username: `Zen`,
-    password: `mitri123`,
-    email: `Mitri@gmail.com`,
+    password: `Zen123`,
+    email: `zen@gmail.com`,
     subscriptionStatus: `VIP`,
-    githubID: `Mitrirer`,
+    githubID: `Zen000`,
     lessonsCompleted: [0, 1, 2, 3],
 },
 ]
  
 function login(email, password) {
-    for (let i = 0; i < users.length; ++i){
-        if (users [i].email === email) { 
+    for (let i = 0; i < users.length; ++i) {
+         if (users[i] .email === email){
             console.log(users[i]);
-            if (users[i].password === password) {
-                console.log (`log the user in - the details are correct`); 
+            if (users[i] .password === password){
+                console.log(`log the user in - the details are correct`);
             }
-        }
-    }
+            else {
+                console.log(`password is incorrect - Try Again`)
+            }
+            return;
+   }
+  }
+  console.log(`could not find an email that matches`)
+ } 
+login(`borer@gmail.com`, `test123`)
+
+
+
+
+function register(user){
+    users.push(user);
 }
-login(`mpborer@gmail.com`, `Test123`)
+
+register({
+   username: `zen`,
+   email: `zen@gmail.com`,
+   password: `zen123`,
+   subscriptionStatus:  `vip`,
+   githubID: `Zen000`,
+   lessonsCompleted: [0, 1, 2, 3]
+});
+
+console.log(users)
